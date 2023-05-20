d62ad25da624928f97b82ed19862a9008a4347ba
Annt <annguyenthe.work@gmail.com>
2023-05-20 13:00:51 +0700

    update src one main thread reactor

--- reactor-server/one_thread.cc
+++ reactor-server/one_thread.cc
@@ -16,48 +16,6 @@
 
 #define SERVER_PORT 9000
 #define MAX_EVENTS 32
-
-int CreateSocketAndListen() {
-  int local_s = socket(AF_INET, SOCK_STREAM, 0);
-  if (local_s < 0) {
-    std::cerr << "Socket() failed";
-    exit(-1);
-  }
-  // Allow socket descriptor to be reuseable
-  int opt = 1;
-  if (setsockopt(local_s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) <
-      0) {
-    std::cerr << "Setsockopt() failed";
-    close(local_s);
-    exit(-1);
-  }
-
-  // Bind the socket
-  struct sockaddr_in addr;
-  memset(&addr, 0, sizeof(addr));
-
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_port = htons(SERVER_PORT);
-  if (bind(local_s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-    std::cerr << "Bind() failed";
-    close(local_s);
-    exit(-1);
-  }
-
-  // Listen back log
-  if (listen(local_s, 32) < 0) {
-    std::cerr << "listen() failed";
-    close(local_s);
-    exit(-1);
-  }
-  std::cout << "[Server] Listening on Port " << SERVER_PORT << std::endl;
-  std::cout << "Waiting for connections ...\n";
-
-  return local_s;
-}
-
-// API connection
 #define NUM_CLIENTS 10
 int clients_fd[NUM_CLIENTS];
 
@@ -72,7 +30,7 @@ int AddConnection(int fd) {
   // get fd = = in client list
   int index = GetConnectionIndex(0);
   if (index == -1) {
-    std::cerr << "Index failed, there are no connections in the array!";
+    std::cerr << "[server] index failed, there are no connections in the array!";
     return -1;
   }
   // add fd to first element = 0
@@ -81,28 +39,79 @@ int AddConnection(int fd) {
 }
 
 int RemoveConnection(int fd) {
-  if (fd < 1) return -1;
+  if (fd < 1)
+    return -1;
   int index = GetConnectionIndex(fd);
   if (index == -1) {
-    std::cerr << "Index failed, there are no connections in the array!";
+    std::cerr << "[server] index failed, there are no connections in the array!";
     return -1;
   }
   clients_fd[index] = 0;
   return close(fd);
 }
 
-void ServerSendWelcomeMsg(int client_fd) {
-  char msg[80];
-  sprintf(msg, "welcome! you are client #%d!\n", GetConnectionIndex(client_fd));
-  send(client_fd, msg, strlen(msg), 0);
+int CreateSocketAndListen() {
+  int local_s = socket(AF_INET, SOCK_STREAM, 0);
+  if (local_s < 0) {
+    std::cerr << "[server] socket() failed";
+    return -1;
+  }
+  // Allow socket descriptor to be reuseable
+  int opt = 1;
+  if (setsockopt(local_s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) <
+      0) {
+    std::cerr << "[server] setsockopt() failed";
+    close(local_s);
+    return -1;
+  }
+
+  // Bind the socket
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(SERVER_PORT);
+  if (bind(local_s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    std::cerr << "[server] bind() failed";
+    close(local_s);
+    return -1;
+  }
+
+  // Listen back log
+  if (listen(local_s, 32) < 0) {
+    std::cerr << "[server] listen() failed";
+    close(local_s);
+    return -1;
+  }
+  std::cout << "[server] listening on port: " << SERVER_PORT << std::endl;
+  std::cout << "[server] waiting for connections ...\n";
+
+  return local_s;
 }
 
-void ReceiveMessages(int server_fd) {
-  char buf[256];
-  int bytes_read = recv(server_fd, buf, sizeof(buf) - 1, 0);
-  buf[bytes_read] = 0;
-  printf("client #%d: %s", GetConnectionIndex(server_fd), buf);
-  fflush(stdout);
+void ServerSendWelcomeMsg(int client_fd) {
+  std::string s = "welcome! you are client #" + std::to_string(GetConnectionIndex(client_fd)) + "\n";
+  int string_length = s.length() + 1;
+  char *char_array = new char[string_length];
+  strcpy(char_array, s.c_str());
+  if (send(client_fd, char_array, string_length, 0) != string_length)
+    std::cerr << "[server] send msg error";
+  delete[] char_array;
+}
+
+void ReceiveMessages(int client_fd) {
+  char buff[256];
+  memset(buff, 0, sizeof(buff));
+  int bytes_read = read(client_fd, buff, sizeof(buff));
+  if (bytes_read < 0) {
+    std::cerr << "[server] read() failed";
+    RemoveConnection(client_fd);
+  } else if (bytes_read == 0) {
+    std::cout << "[client] #" << GetConnectionIndex(client_fd) << " disconnected.";
+    RemoveConnection(client_fd);
+  } else
+    std::cout << "[client] data #" << GetConnectionIndex(client_fd) << ": " << buff;
 }
 
 void EventDemultiplexer(int kq, int local_s) {
@@ -124,7 +133,7 @@ void EventDemultiplexer(int kq, int local_s) {
     int num_events = kevent(kq, NULL, 0, evList, MAX_EVENTS, &tmout);
 
     if (num_events < 0) {
-      std::cerr << "kevent failed";
+      std::cerr << "[server] kevent failed";
       exit(-1);
     } else if (num_events == 0) {
       // std::cout << "Nonblocking!\n";
@@ -137,7 +146,7 @@ void EventDemultiplexer(int kq, int local_s) {
             int fd = accept(evList[i].ident, (struct sockaddr *)&addr,
                             (socklen_t *)&socklen);
             if (fd < 1) {
-              std::cerr << "accept failed";
+              std::cerr << "[server] accept failed";
               close(fd);
               exit(-1);
             }
@@ -145,9 +154,9 @@ void EventDemultiplexer(int kq, int local_s) {
             // Get info client
             getpeername(fd, (struct sockaddr *)&addr,
                         (socklen_t *)&socklen);
-            std::cout << "[+] Connection accepted from IP : "
+            std::cout << "[+] [server] connection accepted from ip: "
                       << inet_ntoa(addr.sin_addr)
-                      << " and port: " << ntohs(addr.sin_port)
+                      << " - port: " << ntohs(addr.sin_port)
                       << std::endl;
 
             // add conection to array
@@ -163,20 +172,21 @@ void EventDemultiplexer(int kq, int local_s) {
           }  // client disconnected
           else if (evList[i].flags & EV_EOF) {
             int fd = evList[i].ident;
-            printf("client #%d disconnected.\n", GetConnectionIndex(fd));
+            std::cout << "[client] #" << GetConnectionIndex(fd) << " disconnected.\n";
+
             EV_SET(&evSet, fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
             kevent(kq, &evSet, 1, NULL, 0, NULL);
+
             RemoveConnection(fd);
-          }  // read message from client
-          else {
+          } else {  // read message from client
             ReceiveMessages(evList[i].ident);
             char msg[80] = "Server send!\n";
             send(evList[i].ident, msg, strlen(msg), 0);
           }
         } else if (evList[i].filter == EVFILT_WRITE) {
-          std::cout << "write!";
+          // std::cout << "write!";
         } else {
-          std::cout << "nothing!";
+          // std::cout << "nothing!";
         }
       }
     }
